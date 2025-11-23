cd35f8d8750cdaf720fb8ac5d17e51dbe71e6b6a
Marcelo Pinotti <marcelopinotti@protonmail.com>
2025-11-23 14:05:17 -0300

    feat: adds parameters to the enums

--- foodstudy-web/src/main/java/com/foodstudy/web/enums/StatusPedido.java
+++ foodstudy-web/src/main/java/com/foodstudy/web/enums/StatusPedido.java
@@ -1,9 +1,22 @@
 package com.foodstudy.web.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum StatusPedido {
-    PENDENTE,
-    CONFIRMADO,
-    RETIRADO,
-    CANCELADO,
-    NAO_RETIRADO
+    PENDENTE("Pendente", false),
+    CONFIRMADO("Confirmado", false),
+    RETIRADO("Retirado", true),
+    CANCELADO("Cancelado", true),
+    NAO_RETIRADO("Não retirado", true);
+
+    private final String descricao;
+    private final boolean finalizado;
+
+    public boolean podeAlterar() {
+        // só deixa alterar se ainda não for finalizado
+        return !finalizado;
+    }
 }
